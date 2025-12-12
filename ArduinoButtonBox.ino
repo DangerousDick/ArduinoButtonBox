373e81a5d9f739510618a8ccefd785709e5fe44b
Richard Sayer <rasayer@gmail.com>
2025-12-12 12:05:43 +0000

    Strip board layout .png file added. Code tidy.

--- ArduinoButtonBox.ino
+++ ArduinoButtonBox.ino
@@ -7,12 +7,6 @@ int button_matrix[matrix_size][matrix_size] = {{0, 1, 2, 3, 4},
                                                {10,11,12,13,14},
                                                {15,16,17,18,19},
                                                {20,21,22,23,24}};
-                                               
-/*int button_matrix_state[matrix_size][matrix_size] = {{0,0,0,0,0},
-                                                     {0,0,0,0,0},
-                                                     {0,0,0,0,0},
-                                                     {0,0,0,0,0},
-                                                     {0,0,0,0,0}};*/
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   (matrix_size * matrix_size), // Button Count
@@ -23,10 +17,8 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   false, false, false);        // No accelerator, brake, or steering
 
 // input pins (column)
-//int col_in_pins[matrix_size] = {2,3,4,5,6};
 int col_in_pins[matrix_size] = {6,5,4,3,2};
 // output pins (row)
-//int row_out_pins[matrix_size] = {8,9,10,11,12};
 int row_out_pins[matrix_size] = {12,11,10,9,8};
 
 void setup()
@@ -56,8 +48,6 @@ void loop()
     for (int col = 0; col < matrix_size; col++)
     {
       delay(5);
-      //button_matrix_state[row][col] = digitalRead(col_in_pins[col]);
-      //Joystick.setButton(button_matrix[row][col], button_matrix_state[row][col]);
       Joystick.setButton(button_matrix[row][col], digitalRead(col_in_pins[col]));
     }
     digitalWrite(row_out_pins[row], LOW);   // no power to pin
